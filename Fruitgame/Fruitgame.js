4c4e25830e24b902bac938529c6b41db8cd365dc
kim tae yang <kimsun1028@naver.com>
2025-11-17 16:28:00 +0900

    사과 div 생성

--- Fruitgame/Fruitgame.js
+++ Fruitgame/Fruitgame.js
@@ -1,5 +1,8 @@
-const startBtn = document.querySelector("#start-btn")
+const startBtn = document.querySelector("#start-btn");
 const gameArea = document.querySelector("#game-area");
+const scoreDisplay = document.querySelector("#score");
+const timerDisplay = document.querySelector("#timer");
+
 let score = 0;
 let timer = 60;
 let timerId = null;
@@ -8,7 +11,7 @@ let timerId = null;
 
 // 게임 시작 함수
 startBtn.onclick = function gameStart() {
-    gamearea.innerHTML = "";
+    gameArea.innerHTML = "";
     score = 0;
     createFruits();
     startTimer();
@@ -17,9 +20,40 @@ startBtn.onclick = function gameStart() {
 
 
 function createFruits() {
+    const rows = 10;
+    const cols = 17;
 
+    for (let i = 0; i < rows; i++){
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
+
+        for (let j = 0; j < cols; j++) {
+            const apple = document.createElement("div");
+            apple.classList.add("apple");
+
+            // 1 ~ 9 랜덤 숫자 부여
+            const num = Math.floor(Math.random() * 9) + 1;
+            apple.textContent = num;
+
+            rowDiv.appendChild(apple);
+        }
+
+        gameArea.appendChild(rowDiv);
+    }
 }
 
 function startTimer() {
-    
+    clearInterval(timerId);
+    timer = 60;
+    timerDisplay.textContent = timer;
+
+    timerId = setInterval(() => {
+        timer--;
+        timerDisplay.textContent = timer;
+
+        if (timer <= 0) {
+            clearInterval(timerId);
+            alert(`게임 종료! 총 점수 : ${score}`);
+        }
+    }, 1000);
 }
